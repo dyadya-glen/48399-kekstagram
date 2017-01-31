7a78ee8df1acb8f013deb8436d4462037682734a
Roman Gulyaev <glen2001@yandex.ru>
2017-01-31 23:27:06 +0300

    #12 Личный проект v2.0

--- js/form.js
+++ js/form.js
@@ -4,72 +4,70 @@ var uploadOverlay = document.querySelector('.upload-overlay');
 var uploadSelectImage = document.getElementById('upload-select-image');
 var uploadFile = document.getElementById('upload-file');
 var uploadFormCancel = uploadOverlay.querySelector('.upload-form-cancel');
+var uploadFilter = document.getElementById('upload-filter');
+var uploadFilterAll = uploadFilter.elements['upload-filter'];
 var filterImagePreview = uploadOverlay.querySelector('.filter-image-preview');
-var uploadFilterAll = uploadOverlay.querySelectorAll('input[name="upload-filter"]');
 var resizeControlsDec = uploadOverlay.querySelector('.upload-resize-controls-button-dec');
 var resizeControlsInc = uploadOverlay.querySelector('.upload-resize-controls-button-inc');
 var resizeControlsValue = uploadOverlay.querySelector('.upload-resize-controls-value');
 
 uploadOverlay.classList.add('invisible');
-
 uploadSelectImage.classList.remove('invisible');
 
 uploadFile.addEventListener('click', openPhotoForm);
-
-uploadFormCancel.addEventListener('click', closedPhotoForm);
-
+uploadFormCancel.addEventListener('click', closePhotoForm);
 resizeControlsDec.addEventListener('click', sizeReduction);
-
 resizeControlsInc.addEventListener('click', sizeIncrease);
+
+for (var j = 0; j < uploadFilterAll.length; j++) {
+  attachHandlers(uploadFilterAll[j]);
+}
 
 function openPhotoForm(event) {
   event.preventDefault();
   uploadOverlay.classList.remove('invisible');
   uploadSelectImage.classList.add('invisible');
-  resizeControlsValue.value = '100%';
-  filterImagePreview.style.transform = 'scale(1)';
+  removeFilter();
+  filterImagePreview.classList.add('filter-none');
+  resizesImage(100);
 }
 
-function closedPhotoForm() {
+function closePhotoForm() {
   uploadOverlay.classList.add('invisible');
   uploadSelectImage.classList.remove('invisible');
 }
 
-for (var j = 0; j < uploadFilterAll.length; j++) {
-  clickControl(uploadFilterAll[j]);
-}
-
-function clickControl(control) {
+function attachHandlers(control) {
   control.addEventListener('click', function () {
-    closedFilters();
-    openFilter(control);
+    removeFilter();
+    filterImagePreview.classList.add('filter-' + control.value);
   });
 }
 
-function closedFilters() {
+function removeFilter() {
   for (var i = 0; i < uploadFilterAll.length; i++) {
-    uploadFilterAll[i].removeAttribute('checked');
     filterImagePreview.classList.remove('filter-' + uploadFilterAll[i].value);
   }
 }
 
-function openFilter(control) {
-  control.setAttribute('checked', true);
-  filterImagePreview.classList.add('filter-' + control.value);
-}
-
 function sizeReduction() {
-  if (parseFloat(resizeControlsValue.value) > 25) {
-    resizeControlsValue.value = parseFloat(resizeControlsValue.value) - 25 + '%';
+  var integerResize = parseFloat(resizeControlsValue.value);
+  if (integerResize > 25) {
+    integerResize -= 25;
+    resizesImage(integerResize);
   }
-  var resize = parseFloat(resizeControlsValue.value) / 100;
-  filterImagePreview.style.transform = 'scale(' + resize + ')';
 }
 
 function sizeIncrease() {
-  if (parseFloat(resizeControlsValue.value) < 100) {
-    resizeControlsValue.value = parseFloat(resizeControlsValue.value) + 25 + '%';
+  var integerResize = parseFloat(resizeControlsValue.value);
+  if (integerResize < 100) {
+    integerResize += 25;
+    resizesImage(integerResize);
   }
-  var resize = parseFloat(resizeControlsValue.value) / 100;
+}
+
+function resizesImage(integerResize) {
+  var resize = integerResize / 100;
   filterImagePreview.style.transform = 'scale(' + resize + ')';
+  resizeControlsValue.value = integerResize + '%';
 }
