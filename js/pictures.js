0830ededb75ea95a86f15e323ea57e7322b49f68
Roman Gulyaev <glen2001@yandex.ru>
2017-02-21 00:17:59 +0300

    #24 Раскладываем по полочкам v1.0

--- js/pictures.js
+++ js/pictures.js
@@ -4,8 +4,40 @@
   var pictureTemplate = document.querySelector('#picture-template');
   var picturesEl = pictureTemplate.content.querySelector('.picture');
   var picturesContainer = document.querySelector('.pictures');
+  var filters = document.querySelector('.filters');
 
   window.load('https://intensive-javascript-server-myophkugvq.now.sh/kekstagram/data', onLoad);
+
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  function sortPicturesRandom(array) {
+    var sorted = [];
+
+    while (sorted.length < 10) {
+      var element = getRandomElement(array);
+
+      var flag = sorted.some(function (value) {
+        return value === element;
+      });
+
+      if (!flag) {
+        sorted.push(element);
+      }
+    }
+
+    return sorted;
+  }
+
+  var sortPicturesComments = function (array) {
+    var sorted = array.slice(0);
+    sorted.sort(function (itemA, itemB) {
+      return itemB.comments.length - itemA.comments.length;
+    });
+
+    return sorted;
+  };
 
   function onLoad(event) {
     var target = event.target;
@@ -17,7 +49,7 @@
       return;
     }
 
-    pictures.forEach(function (picture) {
+    function createsElement(picture) {
       var content = picturesEl.cloneNode(true);
 
       content.querySelector('img').src = picture.url;
@@ -31,8 +63,41 @@
         evt.preventDefault();
         window.showGallery(picture);
       }
-    });
+    }
 
-    picturesContainer.appendChild(fragment);
+    function changeFilter() {
+      picturesContainer.innerHTML = '';
+      picturesContainer.appendChild(fragment);
+    }
+
+    pictures.forEach(createsElement);
+
+    changeFilter();
+
+    function onFiltersElements(e) {
+      var filterEl = e.target;
+
+      if (!filterEl.classList.contains('filters-radio')) {
+        return;
+      }
+
+      switch (filterEl.value) {
+        case 'popular':
+          pictures.forEach(createsElement);
+          break;
+        case 'new':
+          sortPicturesRandom(pictures).forEach(createsElement);
+          break;
+        case 'discussed':
+          sortPicturesComments(pictures).forEach(createsElement);
+          break;
+      }
+
+      changeFilter();
+    }
+
+    filters.addEventListener('click', onFiltersElements);
+
+    filters.classList.remove('hidden');
   }
 })();
